4421d39eec0e84b40bbd5c90bb8081fc00a2bc32
zr2005 <zr2005@hw.ac.uk>
2023-11-06 21:28:41 +0000

    Updated site, added backgrounds and framework. JS.

--- TestWorld-website/Worldscript.js
+++ TestWorld-website/Worldscript.js
@@ -1,4 +1,8 @@
-// document.addEventListener("DOMContentLoaded", function () {
-//     const messageElement = document.getElementById("message");
-//     messageElement.textContent = "Welcome to Style";
-// });
+window.addEventListener('load', function() {         // Event listener for once window is launched
+    const title = document.getElementById('title');
+    title.classList.add('twinkle');            // refer to twinkle class
+
+    setTimeout(function() {        // Timer for 1.2s once tab is launched.
+        title.classList.remove('twinkle');
+    }, 1200); 
+});
